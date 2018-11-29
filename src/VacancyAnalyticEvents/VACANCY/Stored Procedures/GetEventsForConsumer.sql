3897c55319aecfbd08cda12bccd492a4f48b1a56
Sachin Patel <sachin.patel@valtech.co.uk>
2018-11-29 09:49:35 +0000

    :card_file_box: - Initial sql proj for vacancy event store.

---
+++ src/VacancyAnalyticEvents/VACANCY/Stored Procedures/GetEventsForConsumer.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [VACANCY].[GetEventsForConsumer]
+	@VacancyReference BIGINT
+AS
+	SELECT	VacancyReference
+	,		PublisherId
+	,		EventType
+	,		SUM(COUNT(EventType)) OVER(PARTITION BY VacancyReference, PublisherId) AS NoOfOccurancesPerPublisher
+	,		SUM(COUNT(EventType)) OVER(PARTITION BY VacancyReference) AS NoOfOccurances
+	,		Id
+	FROM	[VACANCY].[Event]
+	GROUP
+	BY		VacancyReference
+	,		EventType
+	,		PublisherId
+	,		Id
+	ORDER
+	BY		VacancyReference
+	,		EventType
+	,		PublisherId
+	,		Id
