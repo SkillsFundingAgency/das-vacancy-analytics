ae69a8b89f4d56459511bc314764d75b69ab5018
sachin.patel <sachin.patel@valtech.co.uk>
2018-11-30 16:46:33 +0000

    :hammer: - Correcting name of event time sql table column

--- src/VacancyAnalyticEvents/VACANCY/Tables/Event.sql
+++ src/VacancyAnalyticEvents/VACANCY/Tables/Event.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [VACANCY].[Event] (
     [Id]               UNIQUEIDENTIFIER DEFAULT (newsequentialid()) NOT NULL,
     [PublisherId]      VARCHAR (50)     NULL,
-    [OccuredOn]        DATETIME         NOT NULL,
+    [EventTime]        DATETIME         NOT NULL,
     [VacancyReference] BIGINT           NULL,
     [EventType]        VARCHAR (100)    NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
