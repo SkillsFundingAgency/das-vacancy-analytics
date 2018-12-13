189f7916deda9468e2391cce68060f6165689cf1
Mark Gwilliam <mark.gwilliam@outlook.com>
2018-12-13 07:03:26 +0000

    file reorg and functions sdk downgrade to 1.0.23

--- src/Functions/Esfa.VacancyAnalytics.Functions/Models/VacancyEvent.cs
+++
@@ -1,9 +0,0 @@
-namespace Esfa.VacancyAnalytics.Functions.Models
-{
-	internal struct VacancyEvent
-	{
-		public long VacancyReference { get; set; }
-		public string EventType { get; set; }
-		public string PublisherId { get; set; }
-	}
-}
