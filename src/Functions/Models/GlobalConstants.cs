fcc3f9a9038657e4d652c9548d61766940d5a920
Kola samuel <kola.samuel@digital.education.gov.uk>
2022-02-16 14:11:58 +0000

    Use Azure function storage

---
+++ src/Functions/Models/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esfa.VacancyAnalytics.Functions.Models
+{
+    public static class GlobalConstants
+    {
+        public const string GenerateVacancyAnalyticsQueueName = "generate-vacancy-analytics-summary";
+    }
+}
